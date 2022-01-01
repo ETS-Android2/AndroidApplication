321b17f0fad3c5581530e8a06bf2a041fb112d33
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2022-01-01 13:22:26 +0100

    Maryam_Add_Notification_Snackbar

---
+++ app/src/main/java/com/example/user/navigationdrawersample/MonApp.java
@@ -0,0 +1,34 @@
+package com.example.user.navigationdrawersample;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MonApp extends Application {
+
+    public static final String exampleNotification ="Example Notification";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotifcationExample();
+    }
+
+    private void createNotifcationExample(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notif =new NotificationChannel(
+                    exampleNotification,
+                    "ExampleNotification",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+
+            notif.setDescription("this is an example of Notification");
+
+
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(notif);
+
+        }
+    }
+}
